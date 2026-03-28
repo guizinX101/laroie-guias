f95360c4d9a4414dff0a3c3496b618e3da41e7e4
guizinX101 <guizin2.0elite@gmail.com>
2026-03-28 19:13:11 -0300

    Create ProductCart.tsx

---
+++ src/components/ProductCart.tsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router';
+import { Product } from '../data/products';
+import { ImageWithFallback } from './figma/ImageWithFallback';
+
+interface ProductCardProps {
+  product: Product;
+}
+
+export function ProductCard({ product }: ProductCardProps) {
+
+  return (
+    <Link to={`/produto/${product.id}`} className="group">
+      <div className="bg-card rounded-lg overflow-hidden shadow-md hover:shadow-xl transition-shadow pt-[80px]">
+        <div className="aspect-square overflow-hidden bg-muted">
+          <ImageWithFallback
+            src={product.image}
+            alt={product.name}
+            className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300"
+          />
+        </div>
+        <div className="p-4">
+          <h3 className="mb-2 line-clamp-2 min-h-[3rem]">{product.name}</h3>
+          <div className="flex items-center justify-between">
+            <div>
+              <p className="text-2xl text-foreground">
+                R$ {product.price.toFixed(2)}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
